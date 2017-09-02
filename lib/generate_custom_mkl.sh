fa214157ef22fcfbfb3e29f6c6f0ec4cb497eec8
George Linderman <george.linderman@yale.edu>
2017-09-02 13:30:48 -0400

    changed installname of libiomp5 in the cusotm mkl dylib

--- lib/generate_custom_mkl.sh
+++ lib/generate_custom_mkl.sh
@@ -1,3 +1,6 @@
 make -C /opt/intel/mkl/tools/builder/ libuni export=`pwd`/mkl_fxn_list name=`pwd`/libfastpca_custommkl
 install_name_tool -id "@rpath/libfastpca_custommkl.dylib" libfastpca_custommkl.dylib
 #install_name_tool -id "@rpath/libiomp5.dylib" libiomp5.dylib
+
+#install_name_tool -change libiomp5.dylib @rpath/libiomp5.dylib libfastpca_custommkl.dylib
+#Now, need to change the rpath of libfast_custommkl.dylib to also be @executablelocal../
